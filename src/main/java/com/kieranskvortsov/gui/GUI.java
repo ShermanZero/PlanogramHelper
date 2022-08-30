23389feb529e74ecc641615dcf3cfdf49548c13d
ShermanZero <15kpsherman@gmail.com>
2022-08-30 10:59:00 -0400

    license changes

--- src/main/java/com/kieranskvortsov/gui/GUI.java
+++ src/main/java/com/kieranskvortsov/gui/GUI.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package com.kieranskvortsov.gui;
 
 import com.formdev.flatlaf.FlatDarkLaf;
